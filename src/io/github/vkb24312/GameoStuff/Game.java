7d4740b3c8a1447987f8ce3684f311a993df59d4
Vincent <vincent.bechmann@gmail.com>
2017-05-19 21:28:55 +0300

    Added maven support. I think Please tell me if I didnt. I think I might've done something wrong. IntelliJ decided to add java.main to all my packages. Can someone explain?

--- src/io/github/vkb24312/GameoStuff/Game.java
+++
@@ -1,9 +0,0 @@
-package io.github.vkb24312.GameoStuff;
-
-import org.json.simple.JSONObject;
-
-public class Game {
-    public static void main(JSONObject json){
-
-    }
-}
--- src/io/github/vkb24312/GameoStuff/Item.java
+++
@@ -1,5 +0,0 @@
-package io.github.vkb24312.GameoStuff;
-
-public interface Item {
-    int weight = 1;
-}
