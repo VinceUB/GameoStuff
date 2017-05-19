93e40e81fd529e42abb652633804b7d390b92b85
Vincent <vincent.bechmann@gmail.com>
2017-05-19 20:29:04 +0300

    Changed up the Inventory managers a bit

--- src/io/github/vkb24312/GameoStuff/InventoryManager.java
+++ src/io/github/vkb24312/GameoStuff/InventoryManager.java
@@ -1,6 +1,7 @@
 package io.github.vkb24312.GameoStuff;
 
-import io.github.vkb24312.GameoStuff.Items.Swords.OldSword;
+import io.github.vkb24312.GameoStuff.Items.Weapon;
+import io.github.vkb24312.GameoStuff.Items.Weapons.OldSword;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -15,11 +16,37 @@ public class InventoryManager{
         return objs;
     }
 
-    public static Object itemGetter(String item){
+    public static Object itemToObject(String item){
         if(item.equals("OldSword")){
             return new OldSword();
         } else {
             return null;
         }
+    }
+
+    public static Weapon[] weaponGetter(JSONObject json){
+        JSONObject inv = (JSONObject) json.get("Inventory");
+        int InvSpace = (int) inv.get("InventorySpace");
+        int foo = 0;
+        Weapon[] weapons = new Weapon[InvSpace];
+        weapons = null;
+        for (int i = 0; i < InvSpace; i++) {
+            Item weaapon = (Item) inv.get(Integer.toString(i));
+            if(weaapon instanceof Weapon){
+                weapons[foo] = (Weapon) weaapon;
+                foo++;
+            }
+        }
+        return weapons;
+    }
+
+    /**
+     * @exception this is thrown when the inventory cannot find an item in the inventory
+     */
+    public static class InventoryException extends Exception {
+        public InventoryException() { super(); }
+        public InventoryException(String message) { super(message); }
+        public InventoryException(String message, Throwable cause) { super(message, cause); }
+        public InventoryException(Throwable cause) { super(cause); }
     }
 }
